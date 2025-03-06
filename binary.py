8aa4f073f2a78162c3b70b4ecd8e996f58d51664
jingu_Kim <jso732@naver.com>
2025-03-06 22:35:21 +0900

    binary

---
+++ binary.py
@@ -0,0 +1,19 @@
+T = int(input())
+ 
+for test_case in range(1, T + 1):
+    N = float(input())
+    cnt = 0
+    res = ''
+    while N != 0.0 and cnt <12:
+        N = N *2
+        if N>=1:
+            N = N-1
+            res = res +'1'
+        else:
+            res = res + '0'
+        cnt += 1
+    if cnt >= 12 and N != 0.0:
+        res = 'overflow'
+ 
+ 
+    print(f'#{test_case} {res}')
